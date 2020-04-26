ea8cff4941521acf18bc84f31f27923c9977e358
Junio Iglees Lopes dos Santos <junio.igleesiftm@gmail.com>
2020-04-25 23:35:55 -0300

    tela de login-crud

--- index.php
+++ index.php
@@ -52,6 +52,102 @@ $app->get('/admin/logout', function () {
 	exit;
 
 });
+// FUNÇÃO LOGOUT
+
+//ROTAS DE TELAS COMEÇA AQUI.........
+
+$app->get("/admin/users", function () {
+
+	User::verifyLogin();
+
+	$users = User::listAll();
+	$page = new PageAdmin();
+
+	$page->setTpl("users", array(
+		"users" => $users,
+	));
+
+});
+
+// CREATE =============
+
+$app->get("/admin/users/create", function () {
+
+	User::verifyLogin();
+	$page = new PageAdmin();
+
+	$page->setTpl("users-create");
+
+	$user = new User();
+	$user->setData($user);
+
+});
+
+//DELETE===========================================
+
+$app->get("/admin/users/:iduser/delete", function ($iduser) {
+	User::verifyLogin();
+
+	$user = new User();
+	$user->get((int) $iduser);
+	$user->delete();
+	header("Location: /admin/users");
+	exit();
+
+});
+
+//UPDATE===========================================
+
+$app->get("/admin/users/:iduser", function ($iduser) {
+
+	User::verifyLogin();
+
+	$user = new User();
+	$user->get((int) $iduser);
+
+	$page = new PageAdmin();
+
+	$page->setTpl("users-update", array(
+
+		"user" => $user->getValues(),
+
+	));
+
+});
+
+//CRIANDO ROTA PARA SALVAR DE FATO, QUE É A ROTA POST================
+
+$app->post("/admin/users/create", function () {
+	User::verifyLogin();
+	$user = new User();
+
+	$_POST["inadmin"] = (isset($_POST["inadmin"])) ? 1 : 0;
+	$user->setData($_POST);
+	$user->save();
+	$header("Location: /admin/users");
+	exit;
+	var_dump($user);
+
+});
+
+$app->post("/admin/users/:iduser", function ($iduser) {
+	User::verifyLogin();
+
+	$user = new User();
+	$_POST["inadmin"] = (isset($_POST["inadmin"])) ? 1 : 0;
+	$user->get((int) $iduser);
+	$user->setData($_POST);
+	$user->update();
+
+	header("Location: /admin/users");
+	exit();
+
+});
+
+$app->delete("/admin/users/:iduser", function ($iduser) {
+	User::verifyLogin();
+
+});
 
 $app->run();
 
--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -56,6 +56,71 @@ class User extends Model {
 		$_SESSION[User::SESSION] = NULL;
 
 	}
+
+	public static function listAll() {
+		$sql = new Sql();
+		return $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) ORDER BY b.desperson");
+	}
+
+	public function save() {
+		$sql = new Sql();
+
+		/*
+
+			pdesperson varchar(64),
+			pdeslogin varchar(64),
+			pdespassword varchar(256),
+			pdesemail varchar(128),
+			pnrphone  bigint,
+			pinadmin TINYINT
+		*/
+		$results = $sql->select("CALL sp_users_save(:desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)", array(
+
+			$this->getdesperson(),
+			$this->getdeslogin(),
+			$this->getdespassword(),
+			$this->getdesemail(),
+			$this->getnrphone(),
+			$this->getinadmin(),
+
+		));
+
+		$this->setData($results[0]);
+	}
+
+	public function get($iduser) {
+		$sql = new Sql();
+		$results = $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) WHERE a.iduser = :iduser", array(
+			"iduser" => $iduser,
+		));
+
+		$this->setData($results[0]);
+	}
+
+	public function update() {
+		$sql = new Sql();
+
+		$results = $sql->select("CALL sp_usersupdate_save(:iduser, :desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)", array(
+			$this->getiduser(),
+			$this->getdesperson(),
+			$this->getdeslogin(),
+			$this->getdespassword(),
+			$this->getdesemail(),
+			$this->getnrphone(),
+			$this->getinadmin(),
+
+		));
+
+		$this->setData($results[0]);
+	}
+
+	public function delete() {
+		$sql = new Sql();
+		$sql->query("CALL sp_users_delete(:iduser)", array(
+			":iduser" => $this->getiduser(),
+		));
+	}
+
 }
 
 ?>
